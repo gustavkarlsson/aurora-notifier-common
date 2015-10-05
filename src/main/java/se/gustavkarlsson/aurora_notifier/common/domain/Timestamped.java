c4951e10053db0aa5f69e69018094d4d0442edf1
Gustav Karlsson <gustav.karlsson@gmail.com>
2015-10-05 14:02:01 +0200

    Fleshed out Timestamped with equals, hashCode, toString and a default constructor

--- src/main/java/se/gustavkarlsson/aurora_notifier/common/domain/Timestamped.java
+++ src/main/java/se/gustavkarlsson/aurora_notifier/common/domain/Timestamped.java
@@ -1,9 +1,13 @@
 package se.gustavkarlsson.aurora_notifier.common.domain;
 
-public class Timestamped<T> {
+public final class Timestamped<T> {
 
 	private T value;
 	private long timestamp;
+
+	private Timestamped() {
+		// Required by serialization libraries
+	}
 
 	public Timestamped(T value) {
 		this.value = value;
@@ -21,5 +25,33 @@ public class Timestamped<T> {
 
 	public long getTimestamp() {
 		return this.timestamp;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Timestamped that = (Timestamped) o;
+
+		if (timestamp != that.timestamp) return false;
+		if (value != null ? !value.equals(that.value) : that.value != null) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = value != null ? value.hashCode() : 0;
+		result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
+		return result;
+	}
+
+	@Override
+	public String toString() {
+		return "Timestamped{" +
+				"value=" + value +
+				", timestamp=" + timestamp +
+				'}';
 	}
 }
