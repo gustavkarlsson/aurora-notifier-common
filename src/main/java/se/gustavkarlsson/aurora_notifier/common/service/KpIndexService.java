4b286a408e96982e1918b2a9a4bf2bbe99e0b5b4
Gustav Karlsson <gustav.karlsson@gmail.com>
2016-10-18 18:45:49 +0200

    Updated all dependency versions (breaks binary compatibility)

--- src/main/java/se/gustavkarlsson/aurora_notifier/common/service/KpIndexService.java
+++ src/main/java/se/gustavkarlsson/aurora_notifier/common/service/KpIndexService.java
@@ -1,7 +1,7 @@
 package se.gustavkarlsson.aurora_notifier.common.service;
 
-import retrofit.Call;
-import retrofit.http.GET;
+import retrofit2.Call;
+import retrofit2.http.GET;
 import se.gustavkarlsson.aurora_notifier.common.domain.Timestamped;
 
 public interface KpIndexService {
